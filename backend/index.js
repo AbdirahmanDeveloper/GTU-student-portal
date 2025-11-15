715550aff557405ccf92861cb84bbe5fe7612b05
Abdirahman Abdi <abdirahmanabdi@Abdirahmans-MacBook-Air-2.local>
2025-11-15 20:50:52 +0300

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,28 @@ import authRoutes from './routes/authRoutes.js';
 import timetableRoutes from "./routes/timetableRoutes.js";
 import feeRoute from "./routes/feeRoutes.js";
 import resultsRoute from "./routes/resultRoutes.js";
+import courseRoute from "./routes/courseRoutes.js"
+import session from 'express-session';
+import requestRoute from "./routes/requestRoute.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// SESSION MILDWARE
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "superSecretKey123",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,   // true only if using HTTPS
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000 // 1 day
+    }
+  })
+);
 
 // Middleware
 app.use(cors());
@@ -35,10 +52,15 @@ app.get('/', (req, res) => {
   res.send('🎓 Student Portal Backend is running');
 });
 
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/timetable', timetableRoutes);
 app.use('/api/fees', feeRoute);
 app.use("/api/results", resultsRoute);
+app.use("/api/register-course", courseRoute);
+app.use("/api/request", requestRoute);
+
 
 // Start server
 app.listen(PORT, () => {
